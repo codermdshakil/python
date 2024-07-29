3634aac8c0bf41eb8c94e3ae42ca3761cfcce62a
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-29 22:11:05 +0600

    Learn : take input from user [Note : input() take value in string format]

---
+++ 3_take_input_from_user.py
@@ -0,0 +1,15 @@
+# Input take from user
+user_name = input("Enter your name : ");
+user_age = input("Enter your age : ");
+user_profession = input("Enter what's your Profession : ");
+user_hobbies = input("Enter your hobbies : ")
+
+print("My name is ", user_name, ".", "My age is ", user_age, "My profession is ", user_profession,". ", "My hobbiers is ", user_hobbies, ".");
+
+# print(user_name);
+# print(user_age);
+# print(user_profession);
+# print(user_hobbies);
+ 
+
+# Note : input() always take data string format 
