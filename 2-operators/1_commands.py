3d4404a9794762ab4ca0870f61814a4f337e7be6
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-29 22:28:19 +0600

    Learn : Single and Multi line Commands

---
+++ 2-operators/1_commands.py
@@ -0,0 +1,14 @@
+# Commands
+# Single line commands
+# ---------------------
+print("Single line commands using hash symbol (#)");
+# using # hash symbol we can commands python code - single line commands
+
+print("Multiline commands using three single quatation (''' here code to commands ''' )")
+''' Using three single quotation we can commands multilie'''
+'''
+userName = "Shakil Ahmed"
+userAge = 20
+userProfession = "Teacher"
+
+'''
