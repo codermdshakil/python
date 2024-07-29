fa0a30182403475df22dc60ab8d691001f1719f8
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-29 21:57:32 +0600

    Learn : variables

---
+++ 2_variables.py
@@ -0,0 +1,23 @@
+
+# variables
+
+a =10
+b = 20
+
+name = "Shakil Ahmed"
+_ = "Shakil Ahmed is a programmer"
+
+num1 = 3;
+num2 = 100;
+_professtion = "Teacher | Programmer"
+myList = ['alo', 'peyaj', 'rise', 1.5, 2000];
+
+print(a + b)
+print(name , _professtion)
+print(_)
+print(myList)
+print(myList[0])
+print(myList[1])
+print(myList[2])
+print(myList[3])
+print(myList[4])
