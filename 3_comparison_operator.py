d5b76c69acb27ce0bf61d0f18f5bf9a4d30b2678
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-29 22:54:38 +0600

    Learn : Arithmetic and Comparison Operator

---
+++ 3_comparison_operator.py
@@ -0,0 +1,20 @@
+# Python Comparison Operators
+# == Equal
+# != Not Equal
+# > Greater then
+# >= Greater then or Equal
+# < Less then
+# <= Smaller then or Equal
+
+# Boolean value -- true or false
+
+print(5 > 2) # True
+print(4 > 7) # False
+print(10 >= 10) # Equal or Greater then  [True]
+print(10 <= 10) # Equal or Greater then [True]
+print(10 == 10) # Equal [True]
+print(10 != 10) # Not Equal [False]
+
+
+
+
