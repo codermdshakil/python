f85ae1f0c8747050080acf71197d0012ddc9d52a
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-29 22:41:53 +0600

    Learn : Arithmetic operator

---
+++ 2-operators/2_arithmetic_operators.py
@@ -0,0 +1,27 @@
+# Python Arithmetic Operators
+a = 15
+b = 2
+# + addition
+print(a + b); # 17
+
+# - Substraction
+print(a - b); #13
+
+# * Multipication
+print(a * b); #30
+
+# / Division
+print(a / b); #7.5
+
+# ** Floor Division [just integer]
+print(a // b); #7 
+
+# % Modulas or Remainder
+print(a % b); #1
+
+# ** Exponentiation
+print(2** 3); # 8 [2^3 = 2*2*2 = 8]
+
+
+
+
