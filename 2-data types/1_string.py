d26a16bc1fc51e2f0ff2f76026662c4fcfdba32e
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-30 23:09:25 +0600

    Learn : string creating using single, double and triple quatation

---
+++ 2-data types/1_string.py
@@ -0,0 +1,24 @@
+# What is string?
+# A String is a data structure in Python Programming that represents a sequence of characters. It is an immutable data type, meaning that once you have created a string, you cannot change it. Python String are used widely in many different applications, such as storing and manipulating text data, representing names, addresses, and other types of data that can be represented as text.
+
+'''Create Strings'''
+# 1. using single quatation
+string1 = 'Hello, Shakil Ahmed'
+print(string1)
+
+# 2. using double quatation
+string2 = "Hello, Shakil Ahmed Ki obostha?"
+print(string2)
+
+# 3. using triple quatation
+string3 =  '''My name is shakil ahmed. I want to be a programmer'''
+print(string3)
+
+# 4. using triple quatation create string that allow multiple line
+
+string4=  '''My name is shakil ahmed.
+I want to be a programmer
+also Good Person'''
+print(string4)
+
+
