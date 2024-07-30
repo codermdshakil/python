cae1bcf145f93dfabcc584c04c3c8909eb3f15cc
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-30 23:23:41 +0600

    Learn : access string chatacter in python using positive and negative index

---
+++ 2-data types/string/2_access_string.py
@@ -0,0 +1,27 @@
+# Access string characters
+# String character only can access by interger index integers can positive (left to right) or negative (right to left/ reverse)
+
+# Declare a string1 
+string1 = "Shakil"
+
+# Using Positive integer index access string character left to right assending order
+
+print(string1[0]) # S
+print(string1[1]) # h
+print(string1[2]) # a
+print(string1[3]) # k
+print(string1[4]) # i
+print(string1[5]) # l
+print("First character of string (Assending order using positive index ) is : ", string1[0]); # S
+print("Last character of string (Assending order using positive index ) is : ", string1[5]); # l
+
+
+# Using Negative integer index access string character (right to left) dessending order
+print(string1[-1]) # l
+print(string1[-2]) # i
+print(string1[-3]) # k
+print(string1[-4]) # a
+print(string1[-5]) # h
+print(string1[-6]) # S
+print("First character of string (Dessending order using negative index ) is : ", string1[-6]); # S
+print("Last character of string (Dessending order using negative index )is : ", string1[-1]); # l
