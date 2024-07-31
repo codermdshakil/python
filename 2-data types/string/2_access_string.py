134d6b0b5c78e1e2868ba9dd6eb2050c303553a8
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-31 22:37:10 +0600

    Learn : access string

--- 2-data types/string/2_access_string.py
+++ 2-data types/string/2_access_string.py
@@ -25,3 +25,5 @@ print(string1[-5]) # h
 print(string1[-6]) # S
 print("First character of string (Dessending order using negative index ) is : ", string1[-6]); # S
 print("Last character of string (Dessending order using negative index )is : ", string1[-1]); # l
+
+#  In these two way we can access string
