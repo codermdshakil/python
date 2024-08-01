fb2ab2f773e9655284e0f630bf6715451435b7fd
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 23:12:31 +0600

    Learn : python string methods : upper, lower, title, swapcase, capitalized

---
+++ 2-data types/string/7_string_methods.py
@@ -0,0 +1,18 @@
+text = "Shaki AhMed"
+
+# upper() function to convert string to upper case
+print(text.upper())
+
+#lower() function to convert string to lower case
+print(text.lower())
+
+#title() converts the first character to upper case and rest to lower case 
+print(text.title())
+
+#swapcase() convert upper to lower and lower to upper
+print(text.swapcase())
+
+#capitalize() convert first character Upper in string
+print(text.capitalize())
+
+
