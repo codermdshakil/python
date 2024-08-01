d85c74c7e20ff1cb02de85e7ce1d43447875a339
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 20:24:28 +0600

    Learn : String slicing using colon (:)

---
+++ 2-data types/3_string_operations.py
@@ -0,0 +1,13 @@
+#1. String Slice
+# ----------------
+string0 = "Shakil"
+# Slice character from 2:4
+# print(string0[2:4]) #ak
+# string1 = "My name is Shakil"
+# Slice character from 2:4
+# print(string1[10:17]) #Shakil
+
+# print character from o index to second last index
+# print(string0[1:-2]) # hak
+
+
