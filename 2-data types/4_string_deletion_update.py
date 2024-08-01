332c9999eb9d7cd60a6e9891ed155d5e87e73b9b
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 20:54:44 +0600

    Learn : Update a single character in string and also Update entire string

--- 2-data types/4_string_deletion_update.py
+++ 2-data types/4_string_deletion_update.py
@@ -1,4 +1,5 @@
 # 1. Update a character
+#------------------------
 # strings = "Hello, I'm Shakil"
 # print(strings)
 
@@ -7,6 +8,7 @@
 ### there are following two ways
 
 #1 using list then operation then string
+
 # covert string to list/array
 # list1 = list(strings)
 # # access second element and change value 
@@ -17,14 +19,17 @@
 # print(string2) # Hpllo, I'm Shakil
 
 #2 using slice method
+
 # string3 = strings[0:2] + 'p' + strings[3:] 
-# print(string3) # Heplo, I'm Shakil
+# print(string3) # Heplo, I'm Shakil 
 
 # 2. Update entire string
+#----------------------------
+
 # In Python Programming, As Python strings are immutable in nature, we cannot update the existing string. We can only assign a completely new value to the variable with the same name.
-string1 = "I want to be a teacher"
-string1 = "I want to be a programmer"
-print(string1)
+# string1 = "I want to be a teacher"
+# string1 = "I want to be a programmer"
+# print(string1)
 
 
  
