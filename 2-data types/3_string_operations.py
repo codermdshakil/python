5f07ae36bb09ce491d5ff1ec8899c1ada7d90705
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 20:29:42 +0600

    Learn : reversed using slice method using [::-1]

--- 2-data types/3_string_operations.py
+++ 2-data types/3_string_operations.py
@@ -1,13 +1,17 @@
 #1. String Slice
 # ----------------
-string0 = "Shakil"
+strings = "Shakil"
 # Slice character from 2:4
-# print(string0[2:4]) #ak
+# print(strings[2:4]) #ak
 # string1 = "My name is Shakil"
 # Slice character from 2:4
 # print(string1[10:17]) #Shakil
 
 # print character from o index to second last index
-# print(string0[1:-2]) # hak
+# print(strings[1:-2]) # hak
+
+
+# 2. Reversed string using string slice method
+print(strings[::-1])
 
 
