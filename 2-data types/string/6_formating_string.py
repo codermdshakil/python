1a8f2fccdcb3a0beb536fcc263a92357b6ead6aa
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 22:52:20 +0600

    Learn : default and positional formetting

---
+++ 2-data types/string/6_formating_string.py
@@ -0,0 +1,15 @@
+
+## using formet() string can formetted :
+#  Format method in String contains curly braces {} as placeholders which can hold arguments according to position or keyword to specify the order.
+
+## Print in default order
+string1 = "{} {} {} {}".format('My', 'Name', 'Shakil', 'Ahmed') #here you can use single or double quatation
+# print default order 
+# print(string1)
+
+## positional fomatting
+string2 = "{1} {0} {3} {2}".format("Name", "My", "Ahmed", "Shakil")
+# My name Shakil Ahmed
+# 0   1   2      3      
+# print(string2)
+
