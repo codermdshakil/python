21c6a28f798c69846c8088e13edf7554b8934994
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 20:49:47 +0600

    Learn : update a single character using 2 ways

---
+++ 2-data types/4_string_deletion_update.py
@@ -0,0 +1,25 @@
+# 1. Update a character
+strings = "Hello, I'm Shakil"
+print(strings)
+
+# Updating a character of the String
+## As python strings are immutable, they don't support item updation directly
+### there are following two ways
+
+#1 using list then operation then string
+# covert string to list/array
+# list1 = list(strings)
+# # access second element and change value 
+# list1[1] = 'p'
+# # convert list/array to string 
+# string2 = "".join(list1)
+# # print updated character string 
+# print(string2) # Hpllo, I'm Shakil
+
+#2 using slice method
+# string3 = strings[0:2] + 'p' + strings[3:] 
+# print(string3) # Heplo, I'm Shakil
+
+
+
+ 
