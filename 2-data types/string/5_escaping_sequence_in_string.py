1eabb80110d4c5ca3afb775f0b1b92bf644f85b8
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-01 22:25:41 +0600

    Learn : Print path, escaping blackslash, print with new line

--- 2-data types/string/5_escaping_sequence_in_string.py
+++ 2-data types/string/5_escaping_sequence_in_string.py
@@ -1,7 +1,21 @@
 
-# Escaping single quatation
+## Escaping single quatation
 string1 = 'I\'m Shakil Ahmed'
 print(string1)
-# Escaping double quatation
+
+## Escaping double quatation
 string2 = "I'm \"Shakil\""
 print(string2)
+
+## Printing Paths with the Escaping blackslash
+string3 = "c:\\python\\string\\"
+print(string3)
+
+## Printing Paths with the use of Tab
+string4 = "Hi\tGeeks"
+print(string4)
+
+## Printing path with new line
+string5 = "Shakil\nAhmed"
+print(string5)
+
