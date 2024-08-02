e65359956d8ee0dbb2994be86ea3e0e182dd420b
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-03 01:12:10 +0600

    Learn : Boolean operator or - if any value is true then result is true

---
+++ 2-data types/3-boolean/3_boolean_operators.py
@@ -0,0 +1,20 @@
+
+## Boolean Operators
+# or 
+# and 
+# not
+# == (equivalent)
+# != (not equivalent)
+
+### Or : if one value true then true
+# a = 1
+# b = 2
+# c = 4
+
+# if a> b or b < c:
+#     print("True")
+# else:
+#     print("False")
+
+# if a or b or c:
+#     print("True")
