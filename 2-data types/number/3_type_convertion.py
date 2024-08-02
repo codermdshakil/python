d224254bfd436453466a7f60b3479f3ea96f88e3
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-03 00:06:28 +0600

    Learn : type convertion (int to float) (float to int) (str (character ) to int) (str to float) (int to complex)

--- 2-data types/number/3_type_convertion.py
+++ 2-data types/number/3_type_convertion.py
@@ -12,3 +12,23 @@ print(float(a))
 ## using build in function convert float to int 
 b = 2.6
 print(int(b))
+
+## using build in function convert str(chracter) to int 
+c = '3'
+print(int(c))
+
+## using build in function convert str(chracter) to float 
+d = '5.6'
+print(float(d))
+
+
+e = 5
+print(complex(e)) # (5+0j)
+
+f = 6.5
+print(complex(f)) # (6.5+0j)
+
+# Note: 
+# We can’t convert a complex data type number into int data type and float data type numbers.
+# We can’t apply complex built-in functions on strings.
+
