1540c7fb2bae1de6618e0209d5189581ecadcd2c
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-03 01:29:47 +0600

    Learn : Is Operator

--- 2-data types/3-boolean/3_boolean_operators.py
+++ 2-data types/3-boolean/3_boolean_operators.py
@@ -43,12 +43,28 @@
 #     print("Boolean value is true")
 
 ### Boolean Equal Operator 
+# a = 5
+# b = 10
 
-a = 5
+# if a == b:
+#     print("Yes a and b are Equal")
+# else:
+#     print("No they are not Same!")
+
+
+### Boolean Is Operator
+# The is keyword is used to test whether two variables belong to the same object. The test will return True if the two objects are the same else it will return False even if the two objects are 100% equal.
+
+a = 10
 b = 10
-
-if a == b:
-    print("Yes a and b are Equal")
+if a is b:
+    print("Yes")
 else:
-    print("No they are not Same!")
+    print("No")
 
+# The code first assigns the value 10 to variables x and y. It then compares x and y using the “is” operator and prints True because they refer to the same object. Next, it assigns two separate lists to x and y. It then compares x and y using the “is” operator and prints False because the lists are different objects in memory. 
+
+x = ["a", "b", "c", "d"] 
+y = ["a", "b", "c", "d"] 
+
+print(x is y)
