23f7ff2b4d878627f8a01c4024d3c6fdbcdf1507
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-03 01:06:55 +0600

    Learn : boolean value from expression

---
+++ 2-data types/3-boolean/2_boolean_value_from_expression.py
@@ -0,0 +1,18 @@
+
+# Declare variables
+
+a = 10
+b = 20
+
+# Comparison
+print(a == 20) # False
+
+var1 = 0
+print(bool(var1))  #False
+  
+var2 = 1
+print(bool(var2))  # True
+  
+var3 = -9.7
+print(bool(var3)) # True
+
