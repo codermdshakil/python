b91f6b706de62cb8cf08d06278884e8e70db6be0
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-02 23:59:56 +0600

    Learn : convert int to float and float to int

---
+++ 2-data types/number/3_type_convertion.py
@@ -0,0 +1,14 @@
+# We can convert one number into the other form by two methods (like int to float)  -
+# a = 1.5
+# b = 5
+
+# c = a + b
+# print(c) # 6.5 [here b = 5 it was int but now c is float]
+
+## using build in function convert int to float 
+a = 2
+print(float(a))
+
+## using build in function convert float to int 
+b = 2.6
+print(int(b))
