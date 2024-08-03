07f58c1f16fbfddd7472221262242f3b6b76b32f
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-03 23:32:13 +0600

    Learn : creating list in python list can is a container where we can store differents type of data like interger, string, object, array

---
+++ 2-data types/4-list/1_creating_list.py
@@ -0,0 +1,26 @@
+# What is list ?
+# The list is a sequence data type which is used to store the collection of data. Tuples and String are other types of sequence data types.A single list may contain DataTypes like Integers, Strings, as well as Objects. Lists are mutable, and hence, they can be altered even after their creation.
+
+# Creating a list
+List = []
+# print(List)
+# Creating a list of numbers
+List = [10, 20 ,30, 40 , 60, 'Shakil', 'Noyon', False]
+# print(List)
+
+# Create list and access it using index
+List = ['Shakil', 'Noyon', 'Nadim']
+# print(List)
+# Access using Index
+# print("Fast element of list - ", List[0])
+# print("Secon element of list - ", List[1])
+
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
+# print(List)
+# create list using differents types of value
+List = [1, 2, 4,'Bangla', 6,'desh',8, 9, 10]
+print(List[3])
+print(List[5])
+desh_name = List[3] + List[5]
+print(desh_name)
+
