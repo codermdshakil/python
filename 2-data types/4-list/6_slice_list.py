a97c7ec762999487cf95265e45d4acf81b7f23c7
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-04 23:00:41 +0600

    Learn : slice list specific range [start index : end index], specific index to end index [5:]

---
+++ 2-data types/4-list/6_slice_list.py
@@ -0,0 +1,29 @@
+## Slicing of a List
+# print a specific range of elements from the list, we use the Slice operation. 
+
+## slice a specific range: [ start index : end index]
+## To print elements from beginning to a range, use: [: Index]
+## To print elements from begining to negative range, use: [:-Index]
+## To print elements from a specific Index till the end, use : [Index:]
+## To print elements from a specific negative Index till the end, use  : [-Index:]
+## To print the whole list in reverse order, use  : [::-1]
+
+# Creating a List
+List = ['G', 'E', 'E', 'K', 'S', 'F',
+        'O', 'R', 'G', 'E', 'E', 'K', 'S']
+print("Initial List: ")
+print(List)
+
+# using Slice operation
+# range based slice
+Sliced_List = List[3:8]
+# print(Sliced_List)
+
+# pre-defined point to end
+# a specific index to end
+Sliced_List = List[5:] # 5 to end
+print("\nElements sliced from 5th element till the end: ")
+print(Sliced_List)
+
+
+
