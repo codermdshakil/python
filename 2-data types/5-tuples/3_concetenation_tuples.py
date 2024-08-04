c59d25912cc5646aa781285d0206e261c7b26417
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-05 00:43:04 +0600

    Learn : concetenation

---
+++ 2-data types/5-tuples/3_concetenation_tuples.py
@@ -0,0 +1,14 @@
+## Concetnation of tuples
+# Concetnation of tuples  means join two tuples using '+' operator.Concatenation of tuples is done always from the end of the original tuple
+
+tuples1 = (1,2,3,4,5)
+tuples2 = ('python', 'shakil', 'ahmed')
+# Concatination tuple
+tuples3 = tuples1 + tuples2
+print(tuples3)
+
+# Note- Only the same datatypes can be combined with concatenation, an error arises if a list and a tuple are combined. 
+
+
+
+
