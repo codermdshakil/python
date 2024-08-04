5035129ccdffa8fe410c57b0b8f0511319175cb5
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-04 17:03:15 +0600

    Learn : Access elements using negative index start -1

--- 2-data types/4-list/2_access_element_from_list.py
+++ 2-data types/4-list/2_access_element_from_list.py
@@ -6,9 +6,16 @@
 # print(List[0]) # Shakil
 # print(List[1]) #  Ahmed
 
-List = [['Shakil', 'Ahmed'], ['Nadim', 'Hassan'], ['Noyon', 'Rahman']]
+# List = [['Shakil', 'Ahmed'], ['Nadim', 'Hassan'], ['Noyon', 'Rahman']]
 
-# Access elements from multidimentional list 
-print(List[0][0] , List[0][1]) # Shakil Ahmed
-print(List[1][0] , List[1][1]) # Nadim Hassan
-print(List[2][0] , List[2][1]) # Noyon Rahman
+# # Access elements from multidimentional list 
+# print(List[0][0] , List[0][1]) # Shakil Ahmed
+# print(List[1][0] , List[1][1]) # Nadim Hassan
+# print(List[2][0] , List[2][1]) # Noyon Rahman
+
+## Access elements using Negative Index
+List = [1, 2, 'Shakil', 4, 'For', 6, 'Ahmed']
+print(List[-1]) # Ahmed
+print(List[-2]) # 6
+print(List[-3]) # For
+print(List[-5]) # Shakil
