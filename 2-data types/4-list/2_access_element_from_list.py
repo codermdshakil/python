a9d8f49288549fb26ae08c80ce1ce9039f7dab34
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-04 17:00:06 +0600

    Learn : Access elements from multidiementional list

---
+++ 2-data types/4-list/2_access_element_from_list.py
@@ -0,0 +1,14 @@
+
+# Create a list 
+# List = ['Shakil', 'Ahmed']
+
+# access list elements using index
+# print(List[0]) # Shakil
+# print(List[1]) #  Ahmed
+
+List = [['Shakil', 'Ahmed'], ['Nadim', 'Hassan'], ['Noyon', 'Rahman']]
+
+# Access elements from multidimentional list 
+print(List[0][0] , List[0][1]) # Shakil Ahmed
+print(List[1][0] , List[1][1]) # Nadim Hassan
+print(List[2][0] , List[2][1]) # Noyon Rahman
