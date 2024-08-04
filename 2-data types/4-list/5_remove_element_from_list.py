b5bf95021389440fd78e96e279e6d4f97376f87c
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-04 21:52:05 +0600

    Learn : remove element  using remove(removed_element)

---
+++ 2-data types/4-list/5_remove_element_from_list.py
@@ -0,0 +1,15 @@
+# Removing Elements from the List
+
+### Method 1: Using remove() method
+# Remove() method only removes one element at a time, to remove a range of elements, the iterator is used. The remove() method removes the specified item.
+# remove() function remove just fast searched value
+ 
+my_list = [1,2,3,4,5,6,7,8,5,9,10,11,12]
+print("Inisial :", my_list)
+my_list.remove(5) 
+print("Results : ", my_list) #[1,2,3,4,6,7,8,'[5]',9,10,11,12] // this 5 not remove
+my_list.remove(8)
+print("Results : ", my_list) #[1,2,3,4,6,7,'[5]',9,10,11,12] // this 5 not remove
+
+
+
