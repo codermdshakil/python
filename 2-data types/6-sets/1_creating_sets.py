a9b838825023c906bf3f9b12f00f887ccfffa8c9
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-06 01:45:42 +0600

    Learn : creating sets using list, tuple, dictonary

---
+++ 2-data types/6-sets/1_creating_sets.py
@@ -0,0 +1,25 @@
+## Sets
+
+
+
+# Creating sets with string
+set1 = set("Shakil")
+# print(set1)
+strings = "shakilahmed"
+set1 = set(strings)
+
+
+#Create sets with list
+set1 = set(['shakil', 'noyon', 'nadim'])
+# print(set1)
+
+
+# Creating sets using tuples
+t = tuple( 'shakil')
+set1 = set(t)
+# print(set1)
+
+# creating a sets using dictonary
+d = {'geeks':0,'for':1,'geeks':2 }
+set1 = set(d)
+print(set1)
