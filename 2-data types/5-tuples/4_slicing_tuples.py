8dbdb96e3a9ee7e5ca69206c5f95b5235b5bac59
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-05 23:11:04 +0600

    Learn : ranged based printing using [3:5]

--- 2-data types/5-tuples/4_slicing_tuples.py
+++ 2-data types/5-tuples/4_slicing_tuples.py
@@ -1,9 +1,12 @@
 ### Slice of tuple
 tuples = tuple("Geeksforgeeks")
-# print(tuples)
+print(tuples)
 
 #remove fast elements
 # print(tuples[5:])  # using 5 number remove tuple fast 5 elements
 
 # Reverse tuples
-print(tuples[::-1])
+# print(tuples[::-1])
+
+# printing elements based on range using [start index : where to end index] 
+print(tuples[3:5]) 
