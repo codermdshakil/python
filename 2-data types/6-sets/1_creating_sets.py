54d549f2aa926ef65da9f0604ef116b517d144ff
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-06 01:48:44 +0600

    Learn : create sets using differents way

--- 2-data types/6-sets/1_creating_sets.py
+++ 2-data types/6-sets/1_creating_sets.py
@@ -1,5 +1,8 @@
 ## Sets
 
+# Creating sets
+my_set = {1,3,4,2,5}
+print(my_set)
 
 
 # Creating sets with string
@@ -22,4 +25,14 @@ set1 = set(t)
 # creating a sets using dictonary
 d = {'geeks':0,'for':1,'geeks':2 }
 set1 = set(d)
-print(set1)
+# print(set1)
+
+# Creating a Set with a List of Numbers
+# (Having duplicate values)
+set1 = set([1, 2, 4, 4, 3, 3, 3, 6, 5 , 8 , 7 ,10]) # {1, 2, 3, 4, 5, 6}
+# print(set1)
+
+# Creating a Set with a mixed type of values
+# (Having numbers and strings)
+set1 = set([1, 2, 'Geeks', 4, 'For', 6, 'Geeks']) # {1, 2, 4, 6, 'Geeks', 'For'}
+# print(set1)
