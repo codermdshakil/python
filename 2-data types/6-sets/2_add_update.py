2786fa7168ad13ad8b39f4383dd0fcf6c093ee60
codermdshakil <ahmedshakil0512@gmail.com>
2024-08-06 21:42:05 +0600

    Learn : sets element add

---
+++ 2-data types/6-sets/2_add_update.py
@@ -0,0 +1,15 @@
+
+set1 = set()
+print(set1)
+
+# add elements on set1
+set1.add(8)
+set1.add(5)
+set1.add((5,6))
+print(set1)
+
+# using range based add elements
+for i in range(1, 6):
+    set1.add(i)
+
+print(set1)
