c10edffee659c5f3b3c1a29647518b497bc2f646
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-23 21:01:15 +0600

    Learn : variable in python

--- bismillah.py
+++ bismillah.py
@@ -1,3 +1,22 @@
 
-print("Bismillahir Rahmanir Rahim")
-print("Hello world")
+# Variable #
+#-------------#
+
+
+# A variable is a name given to a memory location in a program
+# variable_name = variable_value
+
+
+name  = "Shakil Ahmed"
+age = 19
+price = 87.56
+
+
+age2 = age;
+
+print( "My name is : ",name)
+print("My age is : ", age2)
+print("Here is the price of my hours : ", price)
+
+
+
