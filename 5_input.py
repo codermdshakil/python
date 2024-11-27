f60974e9e5529ba4d6ee056220beefa08f99f681
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-27 23:23:06 +0600

    Learn : input

---
+++ 5_input.py
@@ -0,0 +1,12 @@
+
+## input() statement is used to accept values (using keyboard) from user
+
+name = input("Name : ")
+age = int(input("Enger Age : "))
+cgpa = float(input("Enter CGPA Point : "))
+result = f"My name is {name} and Age is {age}. My Point is {cgpa}."
+# print(name)
+# print(age)
+print(result)
+
+
