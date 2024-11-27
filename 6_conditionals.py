a72eebd42dcee1e56ad95b720bd5a1f3e6ea24aa
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-27 23:59:13 +0600

    Practice : Condition using 2 examples

--- 6_conditionals.py
+++ 6_conditionals.py
@@ -15,41 +15,47 @@
 
 ## Example 1 : if, elif, else
 
-marks = 75
+# marks = 75
 
-if marks >= 95 and marks <= 100:
-    print("You get golden A+")
-elif marks >= 80 and marks < 95:
-    print("You get A+")
-elif marks >= 70 and marks < 80:
-    print("You get A")
-elif marks >= 60 and marks < 70:
-    print("You get A")
-elif marks >= 50 and marks < 60:
-    print("You get A-")
-elif marks >= 40 and marks < 50:
-    print("You get B")
-elif marks >= 33 and marks < 40:
-    print("You get C") 
-elif marks < 33:
-    print("You are Fail")
-else:
-    print("Enter your number again")
+# if marks >= 95 and marks <= 100:
+#     print("You get golden A+")
+# elif marks >= 80 and marks < 95:
+#     print("You get A+")
+# elif marks >= 70 and marks < 80:
+#     print("You get A")
+# elif marks >= 60 and marks < 70:
+#     print("You get A")
+# elif marks >= 50 and marks < 60:
+#     print("You get A-")
+# elif marks >= 40 and marks < 50:
+#     print("You get B")
+# elif marks >= 33 and marks < 40:
+#     print("You get C") 
+# elif marks < 33:
+#     print("You are Fail")
+# else:
+#     print("Enter your number again")
    
 
 ## Example 2 : if, elif, else
 # Trafic lights code
 
-light = input("light : ")
+# light = input("light : ")
 
-if (light == "red"):
-    print("Stop")
-elif (light == "green"):
-    print("Go")
-elif (light == "yellow"):
-    print("Look")
-else:
-    print("Light is Broken")
+# if (light == "red"):
+#     print("Stop")
+# elif (light == "green"):
+#     print("Go")
+# elif (light == "yellow"):
+#     print("Look")
+# else:
+#     print("Light is Broken")
+    
+    
+    
+    
+
+
     
 """
 AND, OR rules
