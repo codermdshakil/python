cfe96fb0e10d2a14337d4cddbb02d1d654d7db12
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-01 00:01:15 +0600

    Learn : Operator in python

---
+++ 8_operators.py
@@ -0,0 +1,62 @@
+
+
+
+
+# Arithmetic Operator -> +, -, *, /, %, **
+
+# a = 5
+# b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b) # remainder
+# print(a ** b) # Exponensial
+
+
+# Relational/ Comparison Operator -> ==, != ,<, >, >=, <= 
+# comparison operator return TRUE OR FALSE
+
+# a = 50
+# b = 20
+
+# print(a== b) # False
+# print(a != b) # True
+# print( a >= b) # True
+# print(a > b) #True
+# print(a <= b) #False
+
+
+
+#Assignment Operators -> =, +=, -=, *=, /=,%=, **=
+
+# num = 10
+# num += 10
+# num -= 5
+# num *= 10
+# num /= 2
+# num **= 5
+# print(num)
+
+
+## Logical Operators -> NOT, AND, OR
+
+a = 50
+b = 30
+
+# print(not (a > b))
+
+# print(not False)
+# print(not True)
+
+
+val1 = True
+val2 = False
+
+# print("Ans operators: ", val1 and val2) # if one is false result false
+# print("Ans operators: ", val1 or val2)  # if one is true result true
+print("Ans operators: ", (a==b) or (a>b))  # if one is true result true
+
+
+
