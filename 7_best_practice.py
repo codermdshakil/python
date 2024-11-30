60f0747135ff412c420e09c7d653948a3ce090df
codermdshakil <ahmedshakil0512@gmail.com>
2024-11-30 22:21:44 +0600

    Learn : Best practice of writting code

---
+++ 7_best_practice.py
@@ -0,0 +1,17 @@
+"""
+Best practice for code
+* Simple Instructions
+* One instruction per task
+* Short & meaninful variable names
+* Use appropriate comments
+* avoid complex expressions
+"""
+
+num1 = int(input("A : "))
+num2 = int(input("B : "))
+
+sum = num1 + num2
+print(sum)
+
+
+
