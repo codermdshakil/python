02813eb05191b53182742ce4d5900abec3a91673
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-02 21:04:15 +0600

    Practice : if, elif, else

--- Chapter-2/conditionals.py
+++ Chapter-2/conditionals.py
@@ -10,12 +10,35 @@
 # else:
 #     print("Light has problem")
     
-age = 17
+# age = 17
 
-if age>= 18:
-    print("Can Vote")
+# if age>= 18:
+#     print("Can Vote")
+# else:
+#     print("Can't vote")
+
+mark = 65
+
+if mark >= 90:
+    print("Grade A")
+elif mark >= 80 and mark < 90:
+    print("Grade B")
+    
+elif mark >= 70 and mark < 80:
+    print("Grade C")
+    
+elif mark >= 60 and mark < 70:
+    print("Grade D")
+
+elif mark >= 50 and mark < 60:
+    print("Grade E")
+
+elif mark < 50:
+    print("You are Fail")
+
 else:
-    print("Can't vote")
+    print("Enter valid mak")    
+    
     
     
     
