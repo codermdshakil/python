1caeb39c3a76bf20b35708c91857f80ce01cab65
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-02 21:37:37 +0600

    Practice : Check even and odd number

---
+++ Chapter-2/conditionals_practice.py
@@ -0,0 +1,13 @@
+
+# practice -1 check number is even or odd
+
+# number = 8
+
+# if number % 2 == 0:
+#     print("Even")
+# elif number %2 != 0:
+#     print("Odd")
+# else:
+#     print("Enter valid number")
+    
+    
