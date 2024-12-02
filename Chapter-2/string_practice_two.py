6ba5d178cbe4b7a4002875034a833e81addfc313
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-02 12:28:55 +0600

    Practice : String and string functions

---
+++ Chapter-2/string_practice_two.py
@@ -0,0 +1,8 @@
+# Practice - 1
+# name = input("Enter your name : ")
+# print(f"Your name is {name}. Your name length is {len(name)}")
+
+# Practice - 2
+str1 = "Hi, $I am $ symbol. $99.99"
+print(str1.find("$"))
+
