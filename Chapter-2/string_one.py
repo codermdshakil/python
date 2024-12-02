96112a10f66fa72fdf59dadbf15da0979482e924
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-02 12:15:10 +0600

    Learn: string and string slicing

---
+++ Chapter-2/string_one.py
@@ -0,0 +1,31 @@
+
+# str1 = "This is single line string. \nwe are creating in python"
+# str2 = "This is string two. \ntwe are creating in python"
+# # str2 = 'Shakil Ahmed'
+# # str3 = """this multiline string"""
+# # print(str1)
+
+# s1 = "Shakil"
+# s2 = "Ahmed"
+# final_s = s1 + " " + s2
+# print(final_s)
+
+# print(len(final_s))
+
+# # access value using index. string does not modify using index
+# print(s1[1])
+
+## Slicing#
+# -----------
+
+# str1 = "Apna College"
+# print(str1[1:4])
+# print(str1[:4])
+# print(str1[5:12])
+# print(len(str1))
+# print(str1[:])
+# print(str1[-3:-1])
+# print(str1[::-1])
+
+
+
