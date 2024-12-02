1823d5b59d669a2c72008478c65acebbf4ad15c5
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-02 20:50:50 +0600

    Learn : Conditionals

---
+++ Chapter-2/conditionals.py
@@ -0,0 +1,15 @@
+
+# light = "green"
+
+# if(light == "red"):
+#     print("Stop")
+# elif (light == "green"):
+#     print("Go")
+# elif (light == "yellow"):
+#     print("Loo ")
+# else:
+#     print("Light has problem")
+    
+    
+    
+    
